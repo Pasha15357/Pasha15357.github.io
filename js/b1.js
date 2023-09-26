e3a50c832f55cad233002934c9dd91854583dd1e
Pasha15357 <119806730+Pasha15357@users.noreply.github.com>
2023-09-26 19:43:15 +0300

    first commit

---
+++ js/b1.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+      var flag=0;
+        $("#1").click(function(){
+          flag=1;
+          if (flag == 1){
+            $(".image1").delay(1000).slideToggle("slow");
+            $(".image2").slideUp("slow");$(".image3").slideUp("slow");$(".image4").slideUp("slow");$(".image5").slideUp("slow");$(".image6").slideUp("slow");$(".image7").slideUp("slow");$(".image8").slideUp("slow");$(".image9").slideUp("slow");$(".image10").slideUp("slow");
+            }            
+        }); 
+        $("#2").click(function(){
+          flag=1;
+          if (flag == 1){
+            $(".image2").delay(1000).slideToggle("slow");
+            $(".image1").slideUp("slow");$(".image3").slideUp("slow");$(".image4").slideUp("slow");$(".image5").slideUp("slow");$(".image6").slideUp("slow");$(".image7").slideUp("slow");$(".image8").slideUp("slow");$(".image9").slideUp("slow");$(".image10").slideUp("slow");
+            }     
+        });
+        $("#3").click(function(){
+          flag=1;
+          if (flag == 1){
+            $(".image3").delay(1000).slideToggle("slow");
+            $(".image2").slideUp("slow");$(".image1").slideUp("slow");$(".image4").slideUp("slow");$(".image5").slideUp("slow");$(".image6").slideUp("slow");$(".image7").slideUp("slow");$(".image8").slideUp("slow");$(".image9").slideUp("slow");$(".image10").slideUp("slow");
+            }   
+        }); 
+        $("#4").click(function(){
+          flag=1;
+          if (flag == 1){
+            $(".image4").delay(1000).slideToggle("slow");
+            $(".image2").slideUp("slow");$(".image3").slideUp("slow");$(".image1").slideUp("slow");$(".image5").slideUp("slow");$(".image6").slideUp("slow");$(".image7").slideUp("slow");$(".image8").slideUp("slow");$(".image9").slideUp("slow");$(".image10").slideUp("slow");
+            }   
+        });   
+        $("#5").click(function(){
+          flag=1;
+          if (flag == 1){
+            $(".image5").delay(1000).slideToggle("slow");
+            $(".image2").slideUp("slow");$(".image3").slideUp("slow");$(".image4").slideUp("slow");$(".image1").slideUp("slow");$(".image6").slideUp("slow");$(".image7").slideUp("slow");$(".image8").slideUp("slow");$(".image9").slideUp("slow");$(".image10").slideUp("slow");
+            }   
+        }); 
+        $("#6").click(function(){
+          flag=1;
+          if (flag == 1){
+            $(".image6").delay(1000).slideToggle("slow");
+            $(".image2").slideUp("slow");$(".image3").slideUp("slow");$(".image4").slideUp("slow");$(".image5").slideUp("slow");$(".image1").slideUp("slow");$(".image7").slideUp("slow");$(".image8").slideUp("slow");$(".image9").slideUp("slow");$(".image10").slideUp("slow");
+            }   
+        });   
+        $("#7").click(function(){
+          flag=1;
+          if (flag == 1){
+            $(".image7").delay(1000).slideToggle("slow");
+            $(".image2").slideUp("slow");$(".image3").slideUp("slow");$(".image4").slideUp("slow");$(".image5").slideUp("slow");$(".image6").slideUp("slow");$(".image1").slideUp("slow");$(".image8").slideUp("slow");$(".image9").slideUp("slow");$(".image10").slideUp("slow");
+            }   
+        }); 
+        $("#8").click(function(){
+          flag=1;
+          if (flag == 1){
+            $(".image8").delay(1000).slideToggle("slow");
+            $(".image2").slideUp("slow");$(".image3").slideUp("slow");$(".image4").slideUp("slow");$(".image5").slideUp("slow");$(".image6").slideUp("slow");$(".image7").slideUp("slow");$(".image1").slideUp("slow");$(".image9").slideUp("slow");$(".image10").slideUp("slow");
+            }   
+        });   
+        $("#9").click(function(){
+          flag=1;
+          if (flag == 1){
+            $(".image9").delay(1000).slideToggle("slow");
+            $(".image2").slideUp("slow");$(".image3").slideUp("slow");$(".image4").slideUp("slow");$(".image5").slideUp("slow");$(".image6").slideUp("slow");$(".image7").slideUp("slow");$(".image8").slideUp("slow");$(".image1").slideUp("slow");$(".image10").slideUp("slow");
+            }   
+        }); 
+        $("#10").click(function(){
+          flag=1;
+          if (flag == 1){
+            $(".image10").delay(1000).slideToggle("slow");
+            $(".image2").slideUp("slow");$(".image3").slideUp("slow");$(".image4").slideUp("slow");$(".image5").slideUp("slow");$(".image6").slideUp("slow");$(".image7").slideUp("slow");$(".image8").slideUp("slow");$(".image9").slideUp("slow");$(".image1").slideUp("slow");
+            }   
+        });      
+    });
---
+++ js/b2.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+    $(".im1").click(function () {
+        if ($(".imm1").is(":hidden")) {
+            $(".imm1").show("slow");
+        } else {
+            $(".imm1").hide("slow");
+        }
+        return false;
+    });
+
+    $(".im2").click(function () {
+        if ($(".imm2").is(":hidden")) {
+            $(".imm2").show("slow");
+        } else {
+            $(".imm2").hide("slow");
+        }
+        return false;
+    });
+
+    $(".im3").click(function () {
+        if ($(".imm3").is(":hidden")) {
+            $(".imm3").show("slow");
+        } else {
+            $(".imm3").hide("slow");
+        }
+        return false;
+    });
+    $(".im4").click(function () {
+        if ($(".imm4").is(":hidden")) {
+            $(".imm4").show("slow");
+        } else {
+            $(".imm4").hide("slow");
+        }
+        return false;
+    });
+
+    $(".im5").click(function () {
+        if ($(".imm5").is(":hidden")) {
+            $(".imm5").show("slow");
+        } else {
+            $(".imm5").hide("slow");
+        }
+        return false;
+    });
+
+    $(".im6").click(function () {
+        if ($(".imm6").is(":hidden")) {
+            $(".imm6").show("slow");
+        } else {
+            $(".imm6").hide("slow");
+        }
+        return false;
+    });
+    $(".im7").click(function () {
+        if ($(".imm7").is(":hidden")) {
+            $(".imm7").show("slow");
+        } else {
+            $(".imm7").hide("slow");
+        }
+        return false;
+    });
+
+    $(".im8").click(function () {
+        if ($(".imm8").is(":hidden")) {
+            $(".imm8").show("slow");
+        } else {
+            $(".imm8").hide("slow");
+        }
+        return false;
+    });
+
+    $(".im9").click(function () {
+        if ($(".imm9").is(":hidden")) {
+            $(".imm9").show("slow");
+        } else {
+            $(".imm9").hide("slow");
+        }
+        return false;
+    });
+
+    $(".im10").click(function () {
+        if ($(".imm10").is(":hidden")) {
+            $(".imm10").show("slow");
+        } else {
+            $(".imm10").hide("slow");
+        }
+        return false;
+    });
+});
